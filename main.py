69c688f925ab4a5921b0d3965c8d97a8f1054759
Seongyong Park <EuphoricCatface@gmail.com>
2024-10-12 11:18:09 +0900

    Loop through msgs

--- main.py
+++ main.py
@@ -17,12 +17,20 @@ def main():
     client_framer = FramerRTU(ClientDecoder(), [])
     server_framer = FramerRTU(ServerDecoder(), [])
 
-    test_packet = b"\x01\x03\x01\x31\x00\x1E\x95\xF1"
+    test_stream = [
+        b"\x01\x03\x01\x31\x00\x1E\x95\xF1",  # msg 1
+        b"\x01\x03\x02\x2E\x30\xA5\xF0",  # msg 2
+        b"\x01\x03\x01",  # msg 1
+        b"\x31\x00\x1E",  # msg 1
+        b"\x95\xF1\x01\x03\x02",  # msg 1 / msg 2
+        b"\x2E\x30\xA5\xF0",  # msg 2
+    ]
 
-    print("client_framer:")
-    client_framer.processIncomingPacket(test_packet, callback)
-    print("server_framer:")
-    server_framer.processIncomingPacket(test_packet, callback)
+    for data in test_stream:
+        print("client_framer:")
+        client_framer.processIncomingPacket(data, callback)
+        print("server_framer:")
+        server_framer.processIncomingPacket(data, callback)
 
 
 if __name__ == "__main__":
