b7e1891a263b0b7dccfa47bbcb24d94073ebda1d
Seongyong Park <EuphoricCatface@gmail.com>
2024-10-12 13:57:08 +0900

    Add dummy injector in the viewer

--- modbus_parser_viewer.py
+++ modbus_parser_viewer.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtGui import QTextCursor
 
 from modbus_parser_viewer_ui import Ui_ModbusParserViewer
 
@@ -8,3 +9,15 @@ class ModbusParserViewer(QMainWindow):
         super().__init__(parent=parent)
         self.ui = Ui_ModbusParserViewer()
         self.ui.setupUi(self)
+
+    def inject(self, data: bytes):
+        # DUMMY!
+        hex_list = [f"{b:02X}" for b in data]
+        hex_str = " ".join(hex_list)
+        # QPlainTextEdit.appendPlainText() will add a newline before the appending text
+        self.ui.plainTextEdit_Raw.moveCursor(QTextCursor.MoveOperation.End)
+        if self.ui.plainTextEdit_Raw.textCursor().positionInBlock() != 0:
+            self.ui.plainTextEdit_Raw.insertPlainText(" ")
+            self.ui.plainTextEdit_Raw.moveCursor(QTextCursor.MoveOperation.Right)
+        self.ui.plainTextEdit_Raw.insertPlainText(hex_str)
+        self.ui.plainTextEdit_Raw.moveCursor(QTextCursor.MoveOperation.End)
