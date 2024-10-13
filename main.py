61dd6637a53b139c60e4a100fe9c2f9814f2e70f
Seongyong Park <EuphoricCatface@gmail.com>
2024-10-13 17:41:34 +0900

    update test lines

--- main.py
+++ main.py
@@ -30,15 +30,25 @@ class SerialReaderTest:
         self.baudrate = baudrate
 
         self.cur_line = 0
+        # self.test_lines = [
+        #     b"\x01\x03\x01\x31\x00\x01\xD4\x39",  # msg 1
+        #     b"\x01\x03\x02\x2E\x30\xA5\xF0",  # msg 2
+        #     b"\x01\x03\x01",  # msg 1
+        #     b"\x31\x00\x01",  # msg 1
+        #     b"\xD4\x39\x01\x03",  # msg 1 / msg 2
+        #     b"\x02\x2E",  # msg 2
+        #     b"\x30\xA5\xF0",  # msg 2
+        # ]  # TODO: check many packets in one line
         self.test_lines = [
-            b"\x01\x03\x01\x31\x00\x01\xD4\x39",  # msg 1
-            b"\x01\x03\x02\x2E\x30\xA5\xF0",  # msg 2
-            b"\x01\x03\x01",  # msg 1
-            b"\x31\x00\x01",  # msg 1
-            b"\xD4\x39\x01\x03",  # msg 1 / msg 2
-            b"\x02\x2E",  # msg 2
-            b"\x30\xA5\xF0",  # msg 2
-        ]  # TODO: check many packets in one line
+            b"\x01\x03\x01\x31\x00\x02\x94\x38",
+            b"\x01\x03\x04\x30\x31\x32\x33\xF1\x89",  # ReadHoldingRegisters (3)
+            b"\x0B\x10\x00\x12\x00\x02\x04\x0B\x0A\xC1\x02\xA0\xD5",
+            b"\x0B\x10\x00\x12\x00\x02\xE1\x67",  # WriteMultipleRegisters (16)
+            b"\x0B\x04\x00\x0A\x00\x01\x11\x62",
+            b"\x0B\x04\x02\x10\x2F\x6D\x2D",  # ReadInputRegisters (4)
+            b"\x0B\x06\x00\x04\xAB\xCD\x76\x04",
+            b"\x0B\x06\x00\x04\xAB\xCD\x76\x04",  # WriteSingleRegister (6)
+        ]
 
     def read(self):
         rtn = self.test_lines[self.cur_line]
